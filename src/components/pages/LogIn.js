1933b1aa9a116a1b15a25d95a8b502cb1b0ef688
Kycehhh <sabilalaallenkyle@gmail.com>
2024-04-15 18:46:39 +0800

    Refactor LogIn page layout and add login form

--- src/components/pages/LogIn.js
+++ src/components/pages/LogIn.js
@@ -4,13 +4,22 @@ import library from "../../img/library.jpg";
 
 function LogIn() {
   return (
-    <div>
+    <div className ="flex justify-center	items-center">
       <div>
         <img src={library} alt="Library"/>
       </div>
 
       <div className = "form">
-        <h1 className="inblock">Log In</h1>
+        <div className="header text-center ">
+          <h1>Log In</h1>
+          <h2>Welcome Back</h2>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+       
+        <input type="text" placeholder="Email" required></input>
+        <input type="password" placeholder="Password" required></input>
+
+        <button type="submit" class="btn">Login</button>
       </div>
     </div>
   );
