d02526587524a04fd9bd10bfd8d69ea8d4a0d3a5
qeewpi <leyashml@gmail.com>
2024-04-16 12:31:50 +0800

    Update SideNavBar component and add WelcomeHeader and Reminders components to Home.js

---
+++ src/components/WelcomeHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function WelcomeHeader() {
+  return (
+    <div className="bg-white font-semibold p-6 rounded-xl">
+      <h1>
+        <span className="text-gray-500">Welcome,</span> Juan Dela Cruz
+      </h1>
+    </div>
+  );
+}
+
+export default WelcomeHeader;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,6 @@
+import Reminders from "components/Reminders";
 import SideNavBar from "components/SideNavBar";
+import WelcomeHeader from "components/WelcomeHeader";
 import React from "react";
 import NavBar from "../NavBar";
 
@@ -6,11 +8,18 @@ function Home() {
   return (
     <div>
       <NavBar />
-      <div className="">
-        <div className="flex flex-col flex-grow w-3/12 min-h-screen">
+      <div className="flex h-screen">
+        <div className="hidden md:flex flex-col flex-grow w-3/12 md:w-4/12 h-full">
           <SideNavBar />
         </div>
-        <div></div>
+        <div className="px-8 py-16 flex flex-col flex-grow h-full w-full bg-[#EDEDED] gap-y-4">
+          <div className="w-full">
+            <WelcomeHeader />
+          </div>
+          <div className="w-full">
+            <Reminders />
+          </div>
+        </div>
       </div>
     </div>
   );
