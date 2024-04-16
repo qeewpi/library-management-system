4c5ce68a927869a067bc8d95dacc14f72c1bc272
Kycehhh <sabilalaallenkyle@gmail.com>
2024-04-16 17:57:13 +0800

    Update ProfileDetail component styles and placeholders

--- src/components/pages/ProfileDetail.js
+++ src/components/pages/ProfileDetail.js
@@ -2,43 +2,48 @@ import React from 'react';
 
 function ProfileDetail() {
   return (
-    <div className="text-xl bg-white font-semibold p-6 rounded-xl">
+    <div className="text-base bg-white font-semibold p-6 rounded-xl ">
       
-      <div className="flex items-start mb-4">
-        <span className="text-primaryblack mr-4">Username</span>
+      <div className="flex items-start mb-4 border-b-4">
+        <span className="text-primaryblack mr-4 text-base">Username</span>
         <input
           type="text"
-          className="border border-gray-300 rounded-md p-2 flex-grow"
+          className="border border-gray-300 rounded-md p-2 flex-grow text-base"
+          placeholder="Username"
         />
       </div>
 
-      
-      <div className="flex items-start mb-4">
-        <span className="text-primaryblack mr-4">Email Address</span>
+      <div className="flex items-start mb-4 border-b-4">
+        <span className="text-primaryblack mr-4 text-base">Email Address</span>
         <input
           type="text"
-          className="border border-gray-300 rounded-md p-2 flex-grow"
+          className="border border-gray-300 rounded-md p-2 flex-grow text-base"
+          placeholder="Email address"
         />
       </div>
 
-      
-      <div className="flex items-start mb-4">
-        <span className="text-primaryblack mr-4">Update Password</span>
+      <div className="flex items-start mb-4 border-b-4">
+        <span className="text-primaryblack mr-4 text-base">Update Password</span>
         <input
           type="password"
-          className="border border-gray-300 rounded-md p-2 flex-grow"
+          className="border border-gray-300 rounded-md p-2 flex-grow text-base"
+          placeholder="Enter your current password"
+        />
+        <input
+          type="password"
+          className="border border-gray-300 rounded-md p-2 flex-grow text-base"
+          placeholder="Enter your current password"
         />
       </div>
 
-      
-      <div className="flex items-start">
-        <span className="text-primaryblack mr-4">Your Bio</span>
+      <div className="flex items-start border-b-4">
+        <span className="text-primaryblack mr-4 text-base">Your Bio</span>
         <textarea
-          className="border border-gray-300 rounded-md p-2 flex-grow"
+          className="border border-gray-300 rounded-md p-2 flex-grow text-base"
+          placeholder="Enter your bio here."
         />
       </div>
 
-      
       <button
         type="submit"
         className="btn mt-8 w-40 py-4 rounded-xl bg-secondaryBlue text-white hover:bg-secondaryBlue"
