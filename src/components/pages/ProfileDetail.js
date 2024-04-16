95230ce5c55d9f0d2b16ada854fb1f134de95753
Kycehhh <sabilalaallenkyle@gmail.com>
2024-04-16 15:50:05 +0800

    Refactor ProfileDetail component and update Profile page

--- src/components/pages/ProfileDetail.js
+++ src/components/pages/ProfileDetail.js
@@ -1,22 +1,52 @@
-import React from 'react'
+import React from 'react';
 
 function ProfileDetail() {
-return (
+  return (
     <div className="text-xl bg-white font-semibold p-6 rounded-xl">
-        <div className="border-b-4">
-            <h1>
-                <span className="text-primaryblack">Profile</span>
-            </h1>   
-                <span className="text-gray-500 text-base">Update your personal details here.</span>
-        </div>
+      
+      <div className="flex items-start mb-4">
+        <span className="text-primaryblack mr-4">Username</span>
+        <input
+          type="text"
+          className="border border-gray-300 rounded-md p-2 flex-grow"
+        />
+      </div>
 
-        <div className="border-b-4">
-            <h1>
-                <span className="text-primaryblack">Username</span>
-            </h1>
-        </div>
+      
+      <div className="flex items-start mb-4">
+        <span className="text-primaryblack mr-4">Email Address</span>
+        <input
+          type="text"
+          className="border border-gray-300 rounded-md p-2 flex-grow"
+        />
+      </div>
+
+      
+      <div className="flex items-start mb-4">
+        <span className="text-primaryblack mr-4">Update Password</span>
+        <input
+          type="password"
+          className="border border-gray-300 rounded-md p-2 flex-grow"
+        />
+      </div>
+
+      
+      <div className="flex items-start">
+        <span className="text-primaryblack mr-4">Your Bio</span>
+        <textarea
+          className="border border-gray-300 rounded-md p-2 flex-grow"
+        />
+      </div>
+
+      
+      <button
+        type="submit"
+        className="btn mt-8 w-40 py-4 rounded-xl bg-secondaryBlue text-white hover:bg-secondaryBlue"
+      >
+        Update
+      </button>
     </div>
-)
+  );
 }
 
-export default ProfileDetail
+export default ProfileDetail;
