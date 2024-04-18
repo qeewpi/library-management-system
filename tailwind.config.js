7ac8ed05a4fad4b55490e5b2070541c2c6054036
qeewpi <leyashml@gmail.com>
2024-04-18 23:54:09 +0800

    Refactor component structure, remove Flowbite React library, and fix formatting in package.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-const flowbite = require("flowbite-react/tailwind");
-
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", ,],
   theme: {
     extend: {
       colors: {
