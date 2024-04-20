bbd033d0400b2b8ab844cd54d04f83a34b18d013
qeewpi <leyashml@gmail.com>
2024-04-20 11:01:33 +0800

    Update UserList.js and add SearchBar and ButtonGroup components

--- src/components/pages/admin/UserList.js
+++ src/components/pages/admin/UserList.js
@@ -1,4 +1,6 @@
 import AdminWelcome from "components/admin-components/AdminWelcome";
+import { ButtonGroup } from "components/admin-components/ButtonGroup";
+import SearchBar from "components/admin-components/SearchBar";
 import React from "react";
 import { UserTable } from "../../admin-components/UserTable";
 
@@ -9,6 +11,10 @@ function UserList() {
       <div className="bg-white gap-y-4 flex flex-col p-6 rounded-xl">
         <div>
           <h1 className="text-xl">User Table</h1>
+        </div>
+        <div className="flex flex-row justify-between">
+          <SearchBar />
+          <ButtonGroup />
         </div>
         <UserTable />
       </div>
