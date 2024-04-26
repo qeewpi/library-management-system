ff6fb946e42dadeaf68bb5ccfceb34d3ac5b3eb6
Kycehhh <sabilalaallenkyle@gmail.com>
2024-04-26 18:51:12 +0800

    Update welcome messages to dynamically display the current user's name in AdminWelcome.js and WelcomeHeader.js components

--- src/components/admin-components/ProfileDetail.js
+++ src/components/admin-components/ProfileDetail.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import AuthService from "service/AuthService";
+import { useLocation, useNavigate } from "react-router-dom";
 function ProfileDetail() {
+  let navigate = useNavigate();
+  const location = useLocation();
+  const currentUser = AuthService.getCurrentUser();
+
+  const handleLogout = async () => {
+    AuthService.logout();
+    navigate("/log-in");
+  };
   return (
     <div>
       <div className="pb-16 border-b-4">
-        <h1>Jane Dela Cruz</h1>
+        <h1>{currentUser.name}</h1>
         <h1>admin</h1>
       </div>
       <div className="p-4">
@@ -15,7 +24,8 @@ function ProfileDetail() {
 
         <button
           type="submit"
-          className="btn mt-8 w-full py-4 rounded-xl bg-primaryBlue text-white hover:bg-secondaryBlue"
+          className="btn mt-8 w-full py-4 rounded-xl bg-primaryBlue text-white hover:bg-secondaryBlue "
+          onClick={handleLogout}
         >
           Logout
         </button>
